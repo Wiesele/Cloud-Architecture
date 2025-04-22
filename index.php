d8eb9060a6de4d6291bc194d0ac750a39ae36226
Marvin Rothmann <m.rothmann@fburi.den>
2025-04-22 18:47:53 +0200

    code 3

--- index.php
+++ index.php
@@ -2,6 +2,8 @@
     // Get the connection string from the environment
     $connStr = getenv('AZURE_MYSQL_CONNECTIONSTRING');
 
+    var_dump(getenv())
+
     if (!$connStr) {
         die("Environment variable AZURE_MYSQL_CONNECTIONSTRING not set." . $connStr);
     }
